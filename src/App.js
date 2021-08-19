e7a87c3ab591b5bb50bfda1ecf892aa7ef2c20a2
Doug Moreira <dougmoreira@dougs-mbp.home>
2021-08-19 18:23:48 -0400

    adding new axios routes

--- src/App.js
+++ src/App.js
@@ -8,18 +8,19 @@ function App() {
 
   const getUsers = ()=>{
     console.log('users came here')
-  //     axios.get('http://localhost:8000')
-  //     .then((repsonse)=> setUsers(response.data),
-  //     (err) => console.error(err)
-  //     )
-  //     .catch((error)=> console.error(erro))
+      axios
+      .get('https://datinggameapp.herokuapp.com/api/users')
+      .then((repsonse)=> setUsers(response.data),
+      (err) => console.error(err)
+      )
+      .catch((error)=> console.error(erro))
   }
   useEffect(()=>{
-    // getUsers()
+     getUsers()
   })
   const handleCreate = (user) =>{
     axios
-      .post('http://localhost:8000')
+      .post('https://datinggameapp.herokuapp.com/api/users')
       .then((response)=>{
         console.log(response)
         getUsers()
@@ -27,20 +28,32 @@ function App() {
   }
   const handleUpdate = (user) =>{
     axios
-      .post('http://localhost:8000')
+      .put('https://datinggameapp.herokuapp.com/api/users'+ user.id, user)
       .then((response)=>{
         console.log(response)
         getUsers()
       })
   }
-  const handleDelete = ()=>{
-    
+  const handleDelete = (user)=>{
+    axios
+      .delete('https://datinggameapp.herokuapp.com/api/users' + user.id)
+      .then((response)=>{
+        console.log(response)
+        getUsers()
+      })
   }
+
   return (
     <>
       <h1>Welcome to The Dating Game!</h1>
       <Create handleCreate={handleCreate}/>
-      <Edit handleUpdate={handleUpdate}/>
+      {users.map((user)=>{
+        <div>
+          <h4>Username</h4>
+          <Edit handleUpdate={handleUpdate} user={user}/>
+          <button onClick={handleDelete}>DELETE</button>
+        </div>
+      })}
     </>
   )
 }
